8c689666bf933bc2bbef398f64101194042934af
Sahebrao-Dugane <Sahebdugane123@gmail.com>
2024-06-29 11:55:40 +0530

    initial comment

--- src/main/java/com/demo/controller/StudentController.java
+++ src/main/java/com/demo/controller/StudentController.java
@@ -2,18 +2,14 @@ package com.demo.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.demo.entity.Student;
 import com.demo.service.StudentService;
@@ -75,15 +71,12 @@ public class StudentController {
 	{
 		System.out.println(e);
 		studentService.addStudent(e);
-	
-		
 		return "add_student";
 	}
 	
     @GetMapping("/edit/{id}")
 	public String edit(@PathVariable int id,Model m)
 	{
-		
 		Student e=studentService.getStudentById(id);
 		m.addAttribute("students",e);
 		
@@ -97,8 +90,7 @@ public class StudentController {
 		return "redirect:/";
 	}
     
-   
-    @GetMapping("/delete/{id}")
+   @GetMapping("/delete/{id}")
     public String deleteStudentString(@PathVariable int id) {
         studentService.deleteStudent(id);
         return "redirect:/";
--- src/main/java/com/demo/service/StudentService.java
+++ src/main/java/com/demo/service/StudentService.java
@@ -1,13 +1,44 @@
 package com.demo.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.demo.entity.Student;
+import com.demo.repository.StudentRepository;
 
-public interface StudentService {
-	 List<Student> getAllStudents();
-	    Student saveStudent(Student student);
-	    com.demo.model.Student getStudentById(Long id);
-	    void deleteStudent(Long id);
-		com.demo.model.Student saveStudent(com.demo.model.Student student);
+@Service
+public class StudentService {
+	
+	
+	 @Autowired 
+	 StudentRepository studentRepository;
+	 
+	 public void addStudent(Student e)
+	 {
+		 studentRepository.save(e);
+	 }
+	 
+	public List<Student> getAllStudents()
+	{
+		return studentRepository.findAll();
+		
+		
+	}
+	
+	public Student getStudentById(int id)
+	{
+		Optional<Student> e=studentRepository.findById(id);
+		if(e.isPresent())
+		{
+			return e.get();
+		}
+		return null;
+	}
+	public void deleteStudent(int id) {
+		studentRepository.deleteById(id);
+	}
+
 }
